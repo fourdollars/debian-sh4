ba2fcd9eec272343df29c80715d613693b2a4e1c
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2014-05-10 23:50:40 +0800

    Initial Debian repository script for wheezy-sh4.

---
+++ build.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+# -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*-
+#
+# Copyright (C) 2014 Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+for i in /var/lib/lxc/wheezy-sh4/rootfs/root/*.deb; do
+    reprepro -Vb . includedeb wheezy $i
+done
+
+for i in /var/lib/lxc/wheezy-sh4/rootfs/root/*.dsc; do
+    reprepro -Vb . includedsc wheezy $i
+done
+
+#for i in /var/lib/lxc/wheezy-sh4/rootfs/root/*.changes; do
+#    reprepro -Vb . include wheezy $i
+#done
+
+# vim:fileencodings=utf-8:expandtab:tabstop=4:shiftwidth=4:softtabstop=4
